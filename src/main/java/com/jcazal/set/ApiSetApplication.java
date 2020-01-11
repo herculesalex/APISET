72ab66ab01741822c2f7bd315fe728d31c9e00d5
jose <josecazal.1991@gmail.com>
2020-01-11 20:04:30 -0300

    subida de la api

--- src/main/java/com/jcazal/set/ApiSetApplication.java
+++ src/main/java/com/jcazal/set/ApiSetApplication.java
@@ -1,13 +1,24 @@
 package com.jcazal.set;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class ApiSetApplication {
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ApiSetApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSetApplication.class, args);
 	}
-
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(ApiSetApplication.class);
+	    }
 }
